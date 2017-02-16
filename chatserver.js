10b7b6386f1b5259ae656a3e2649e996919afcfa
Ilya Shusterman <ilyashusterman@outlook.com>
2017-02-16 14:12:27 +0200

    express

--- chatserver.js
+++ chatserver.js
@@ -29,7 +29,7 @@ io.on('connection', function (client) {
      //  client.emit("messages", nickname+": "+ message);
    });
 });
-//some change
+//some changes
 app.use(express.static(__dirname));
 app.get('/', function (req, res) {
     res.sendFile(__dirname+'/index.html');
