6959158df288ad39bc8f615a9d2372b731ce7248
Ilya Shusterman <ilyashusterman@outlook.com>
2017-02-17 19:40:57 +0200

    express

--- chatserver.js
+++ chatserver.js
@@ -5,6 +5,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var ip;
+
+
 //var request = require('request');
 // io.on('connection', function(client){
 //     console.log('Client connected...');
@@ -25,7 +28,8 @@ io.on('connection', function (client) {
    client.on('messages', function (message) {
        var nickname = client.nickname;
        if (nickname === "" || nickname === undefined){
-           nickname = "undefined";
+           nickname = ip+" is undefined";
+           console.log(ip+" connected");
        }
        client.broadcast.emit("message", nickname+" : "+ message);
        io.emit('messages', nickname+" "+": "+ message);
@@ -35,6 +39,7 @@ io.on('connection', function (client) {
 //some changes
 app.use(express.static(__dirname));
 app.get('/', function (req, res) {
+    ip = req.headers['x-forwarded-for'];
     res.sendFile(__dirname+'/index.html');
 });
 
